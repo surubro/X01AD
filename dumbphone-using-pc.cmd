c413433945b74026fb71261a92d0275c661dd60a
surubro <chicken3301@gmail.com>
2025-11-14 05:19:34 +0530

    Update dumbphone-using-pc.cmd

--- dumbphone-using-pc.cmd
+++ dumbphone-using-pc.cmd
@@ -21,3 +21,4 @@ adb shell pm uninstall -k --user 0 com.asus.loguploader
 adb shell pm uninstall -k --user 0 com.asus.loguploaderproxy
 adb shell pm uninstall -k --user 0 com.android.vending
 adb shell pm uninstall -k --user 0 com.google.android.gmssss
+adb shell cmd package install-existing com.google.android.gmsss
